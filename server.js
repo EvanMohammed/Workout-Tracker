7698395b8500795609db0907d5839965ee83f139
Evan <evanmohammedsaleh@gmail.com>
2020-09-10 14:55:59 -0500

    got front api working

--- server.js
+++ server.js
@@ -1,34 +1,49 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 3001;
-
+const path = require("path");
+const PORT = process.env.PORT || 3000;
 const db = require("./models");
-
+const { Workout } = require("./models");
 const app = express();
-
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
-
-
-app.post("/api/workouts" , ({body}, res)=> {
-    db.Cardio.create(body)
-    .then(({_id}) => db.Workout.findOneAndUpdate({}, {$push: {cardio: _id}}, {new:true}))
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
-    .catch(err => {
-        res.json(err);
-    })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
 });
-app.get("")
-
-app.listen(PORT , () => {
-console.log("Listening on PORT " + PORT)
-})
+app.post("/api/workout", (req, res) => {
+  db.Workout.create({}).then((data) => console.log(res.json(data)));
+});
+app.get("/api/workout", (req, res) => {
+  Workout.find()
+    .then((dbworkout) => {
+      res.json(dbworkout);
+    })
+    .catch((err) => {
+      res.json.err;
+    });
+});
+app.put("/api/workout/:id", ({ body }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+  
+});
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
+app.listen(PORT, () => {
+  console.log(`App running on port http://localhost:${PORT}`);
+});
